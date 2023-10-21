2676f2d9b619968f7bee94b69697a900bfb482eb
fedeghe <fedeghe@gmail.com>
2023-10-21 19:27:07 +0200

    reduce

--- src/unicodeist/src/index.js
+++ src/unicodeist/src/index.js
@@ -2,7 +2,8 @@
     var script = document.currentScript,
 
         // must be in synch with src/constants.js
-        FONT_FAMILIES_REDUCTION_MAP = {
+        // FONT_FAMILIES_REDUCTION_MAP
+        FFRM = {
             ar: 'Arial',
             v: 'Verdana',
             ta: 'Tahoma',
@@ -13,34 +14,45 @@
             c: 'Courier New',
             b: 'Brush Script MT'
         },
+        lbs = {
+            r:'rotate',
+            s:'scale',
+            f:'font',
+            c:'color',
+            d:'deg',
+            po:'position',
+            cn:'center',
+            p:'px',
+            t:'transform'
+        },
+        map = {
+            w: function (v) { return 'width:' + v + lbs.p; },
+            h: function (v) { return 'height:' + v + lbs.p; },
+            bgc: function (v) { return 'background-' + lbs.c + ':' + v; },
+            zi: function (v) { return 'z-index:' + v; },
+            c: function (v) { return lbs.c + ':' + v; },
+            ff: function (v) { return lbs.f + '-family:' + FFRM[v]; },
+            fw: function (v) { return lbs.f + '-weight:' + v; },
+            o: function (v) { return 'opacity:' + v; },
+            s: function (v) { return v !== 1 ? lbs.s + '(' + v + ')' : ''; },
+            sx: function (v) { return v !== 1 ? lbs.s + 'X(' + v + ')' : ''; },
+            sy: function (v) { return v !== 1 ? lbs.s + 'Y(' + v + ')' : ''; },
+            rx: function (v) { return v ? lbs.r + 'X(' + v + lbs.d + ')' : ''; },
+            ry: function (v) { return v ? lbs.r + 'Y(' + v + lbs.d + ')' : ''; },
+            rz: function (v) { return v ? lbs.r + 'Z(' + v + lbs.d + ')' : ''; }
+        },
         createElement = function (sty, cnt) {
             var node = document.createElement('div'),
                 styles = [cnt
-                    ? 'position:absolute;transform-origin:center center'
-                    : 'position:relative;overflow:hidden'
+                    ? lbs.po + ':absolute;' + lbs.t + '-origin:' + lbs.cn + ' ' + lbs.cn
+                    : lbs.po + ':relative;overflow:hidden'
                 ],
-                map = {
-                    w: function (v) { return 'width:' + v + 'px'; },
-                    h: function (v) { return 'height:' + v + 'px'; },
-                    bgc: function (v) { return 'background-color:' + v; },
-                    zi: function (v) { return 'z-index:' + v; },
-                    c: function (v) { return 'color:' + v; },
-                    ff: function (v) { return 'font-family:' + FONT_FAMILIES_REDUCTION_MAP[v]; },
-                    fw: function (v) { return 'font-weight:' + v; },
-                    o: function (v) { return 'opacity:' + v; },
-                    s: function (v) { return v !== 1 ? 'scale(' + v + ')' : ''; },
-                    sx: function (v) { return v !== 1 ? 'scaleX(' + v + ')' : ''; },
-                    sy: function (v) { return v !== 1 ? 'scaleY(' + v + ')' : ''; },
-                    rx: function (v) { return v ? 'rotateX(' + v + 'deg)' : ''; },
-                    ry: function (v) { return v ? 'rotateY(' + v + 'deg)' : ''; },
-                    rz: function (v) { return v ? 'rotateZ(' + v + 'deg)' : ''; }
-                },
                 k;
 
             for (k in sty) {
                 if (k === 't') {
                     var trans = [
-                        'transform:translate(' + sty.t.trn[0] + 'px,' + sty.t.trn[1] + 'px)'
+                        lbs.t + ':translate(' + sty.t.trn[0] + lbs.p + ',' + sty.t.trn[1] + lbs.p + ')'
                     ];
                     's' in sty.t && trans.push(map.s(sty.t.s));
                     'sx' in sty.t && trans.push(map.sx(sty.t.sx));
