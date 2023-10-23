44d76fa944503590b919dbebfb93122bbbaa8778
fedeghe <fedeghe@gmail.com>
2023-10-24 00:35:15 +0200

    not yet

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('no tests yet', () => {
     render(<App />);
     const linkElement = screen.getByText(/hello/i);
     expect(linkElement).toBeInTheDocument();
