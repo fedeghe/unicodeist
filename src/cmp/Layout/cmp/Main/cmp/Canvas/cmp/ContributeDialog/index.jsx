2eab6052d1732026e27182238685b0f30a4bcfab
fedeghe <fedeghe@gmail.com>
2023-10-24 11:24:26 +0200

    one

--- src/cmp/Layout/cmp/Main/cmp/Canvas/cmp/ContributeDialog/index.jsx
+++ src/cmp/Layout/cmp/Main/cmp/Canvas/cmp/ContributeDialog/index.jsx
@@ -23,16 +23,12 @@ const CopyDialog = ({visibility, setVisibility}) => {
                     <h3>Hi</h3>
                     <p>I are thrilled to invite you to contribute to this project.</p>
                     <p>Your valuable contributions can help me to enhance the project and make it better.</p>
-                    <p>Whether you have ideas, suggestions, or expertise in coding, we welcome you to contribute by filing issues or opening pull requests.</p>
-                    
+                    <p>Whether you have ideas, suggestions, or expertise in coding, I welcome you to contribute by filing issues or opening pull requests.</p>                    
                     <p>To get started, please visit the <a onClick={onContribute} rel='noreferrer' href="javascript:;" target="_blank"> project repository on GitHub</a> and explore the existing issues or codebase.</p>
                     <p>If you come across any issues or have ideas for improvements, feel free to file an issue or open a pull request. I`ll do my best to review and provide feedback promptly.</p>
-
-<p>Thank you for considering contributing to thi project.</p>
-
-<p>Best regards,</p>
-
-<p>Federico</p>
+                    <p>Thank you for considering contributing to thi project.</p>
+                    <p>Best regards,</p>
+                    <p>Federico</p>
                     
                 </div>
             </Dialog>
