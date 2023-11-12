868efd54cd15a5cc13be1bb5fa40e6aee4bebed1
fedeghe <fedeghe@gmail.com>
2023-11-12 18:51:49 +0100

    show font weight in element

--- src/cmp/Layout/cmp/Panel/cmp/Symbols/Sym/cmp/Element/styles.js
+++ src/cmp/Layout/cmp/Panel/cmp/Symbols/Sym/cmp/Element/styles.js
@@ -47,7 +47,8 @@ export default createUseStyles(theme => ({
     Char: ({ sym : {
         color, rotationX, rotationY, rotationZ,
         scaleX, scaleY, skewX, skewY,
-        additionalStyles = '', blur = 0, opacity
+        additionalStyles = '', blur = 0, opacity,
+        fontWeight
     }}) =>  ({
         color,
         ...mergeAdditionalStyles({
@@ -63,6 +64,7 @@ export default createUseStyles(theme => ({
             `skew(${skewX}deg,${skewY}deg)`
         ].join(' '),
         // filter: blur ? `blur(${blur}px)` : '',
+        fontWeight,
         opacity: opacity,
         fontSize:'2em'
     })
