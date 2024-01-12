0b335a409cb54d9c10010b14fa6cf7d11075c67a
fedeghe <fedeghe@gmail.com>
2024-01-12 20:32:41 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('no tests yet', () => {
+test('no tests yet', () => {
     render(<App />);
     const linkElement = screen.getByText(/Width/i);
     expect(linkElement).toBeInTheDocument();
