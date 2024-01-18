0793b5bdd0f6cb3543e5dbcbe68d1d0c704012eb
fedeghe <fedeghe@gmail.com>
2024-01-18 23:13:25 +0100

    rem due to Channeljs

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('no tests yet', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Width/i);
-    expect(linkElement).toBeInTheDocument();
-});
